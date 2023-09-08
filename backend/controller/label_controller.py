46c7e6ce5b8f9cc32c030640d4bc20d8a8b74b0e
Chenzhusi <327774809@qq.com>
2023-09-08 23:59:13 +0800

    left 11 API

--- backend/controller/label_controller.py
+++ backend/controller/label_controller.py
@@ -6,6 +6,31 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_http_methods
 from django.http import HttpResponse, JsonResponse
 
+from backend.models import CtValidation, CtInformation
+
+
+@csrf_exempt
+@require_http_methods(["POST"])
+def upload_label(dataset, request):
+    patient_id = request.POST.get('description', None)
+    dia_list = request.POST.get('diaList', None)
+    photo_id = request.POST.get('photoId', None)
+    description = request.POST.get('description', None)
+    bone_name = request.POST.get('boneName', None)
+    direction = request.POST.get('direction', None)
+    type_ = request.POST.get('type', None)
+    position = request.POST.get('position', None)
+    ct_validation_po = CtValidation(patient_id=patient_id, photo_id=photo_id, dia_list=dia_list, description=description, bone_name=bone_name, direction=direction, type=type_, position=position, dataset=dataset)
+    ct_validation_po.save()
+
+    ct_information_po = CtInformation.objects.get(patient_id=patient_id, photo_id=photo_id, dataset=dataset)
+    ct_information_po.status = 1
+    ct_information_po.dia_list = dia_list
+    ct_information_po.save()
+
+    return HttpResponse("success")
+
+
 @csrf_exempt
 @require_http_methods(["POST"])
 def done_labeling(dataset, request):
--- backend/urls.py
+++ backend/urls.py
@@ -6,18 +6,18 @@ from .controller import dataset_controller, label_controller
 urlpatterns = [
     # 加上每个 Controller文件开头的 @RequestMapping("XXX")
     # 格式：'XXX/YYY'，前后没有'/'
-    # name一般是YYY，出错可以参考前端 vue文件
-    path('', views.login, name='login'),
-    path('index', views.index, name='index'),
-    path('user/login', views.login, name='login'),
+    path('', views.login),
+    path('index', views.index),
+    path('user/login', views.login),
 
     # dataset_controller
-    path('dataSets', dataset_controller.get_datasets, name='dataSets'),
-    path('dataSetsNames', dataset_controller.get_all_names, name='dataSetsNames'),
-    path('dataSetsNamesLabeled', dataset_controller.get_all_names_labeled, name='dataSetsNamesLabeled'),
-    path('addDataset', dataset_controller.add_dataset, name='addDataset'),
-    path('upload', dataset_controller.upload, name='upload'),
+    path('dataSets', dataset_controller.get_datasets),
+    path('dataSetsNames', dataset_controller.get_all_names),
+    path('dataSetsNamesLabeled', dataset_controller.get_all_names_labeled),
+    path('addDataset', dataset_controller.add_dataset),
+    path('upload', dataset_controller.upload),
 
     # label_controller
-    path('doneLabeling/<str:dataset>/', label_controller.done_labeling, name='doneLabeling')
+    path('vqa/<str:dataset>', label_controller.upload_label),
+    path('doneLabeling/<str:dataset>', label_controller.done_labeling),
 ]
